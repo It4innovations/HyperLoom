ab49d5b122492715ae99f4b2d33fde16bbb62524
Stanislav Bohm <stanislav.bohm@vsb.cz>
2016-12-02 10:42:40 +0100

    ENH: python/setup.py added

---
+++ python/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='loom',
+      version='1.0',
+      description='Python interface for Loom - workflow system',
+      author='Loom team',
+      url='',
+      packages=['loom'])
