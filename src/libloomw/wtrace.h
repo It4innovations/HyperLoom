05c0cbfc6a8e1a9950bd539e8341de026fa70f53
Tom Vander Aa <vanderaa@imec.be>
2017-06-02 10:24:49 +0200

    FIX: std::__cxx11::string -> std::string

--- src/libloomw/wtrace.h
+++ src/libloomw/wtrace.h
@@ -14,8 +14,8 @@ public:
     void trace_task_started(const Task &task);
     void trace_task_finished(const Task &task);
     void trace_monitoring();
-    void trace_send(base::Id id, size_t size, const std::__cxx11::string &target);
-    void trace_receive(base::Id id, size_t size, const std::__cxx11::string &target);
+    void trace_send(base::Id id, size_t size, const std::string &target);
+    void trace_receive(base::Id id, size_t size, const std::string &target);
 
 private:
     uint64_t last_monitoring_time;
