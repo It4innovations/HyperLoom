48710942bb50b3717a5eea5ee792b86a197dc4c6
Vojtech Cima <vojtechcima@w118-209.vsb.cz>
2017-06-08 09:19:57 +0200

    FIX: python module installation
    Use setuptools find_packages() to install submodules recursively

--- python/setup.py
+++ python/setup.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 
-from distutils.command.build_py import build_py as _build_py
-from distutils.core import setup
+from setuptools.command.build_py import build_py as _build_py
+from setuptools import setup, find_packages
+
+
 import os
+
 
 def read_version():
     with open(os.path.dirname(__file__) + "../version") as f:
         return f.readline().rstrip()
 
-LOOM_VERSION=read_version()
+
+LOOM_VERSION = read_version()
 print("LOOM_VERSION =", LOOM_VERSION)
 
 
@@ -19,12 +23,11 @@ class build_protoc(_build_py):
         _build_py.run(self)
 
 
-
 setup(name='loom',
       version=LOOM_VERSION,
       description='Python interface for Loom - workflow system',
       author='Loom team',
       url='',
-      packages=['loom'],
+      packages=find_packages(),
       cmdclass={'build_py': build_protoc}
       )
